6098800d455f508be00adc0fb46857cf45022bbf
git-anup <88287032+git-anup@users.noreply.github.com>
2022-06-09 00:26:56 +0530

    Create fibo_gen_async_rst_tb.v

---
+++ Fibonacci Series Generator/Experiment 2/fibo_gen_async_rst_tb.v
@@ -0,0 +1,29 @@
+module fibo_gen_async_rst_tb ();
+  
+  reg clk, reset;
+  wire [3:0] out;
+    
+  fibo_gen_async_rst DUT (.clk(clk), .reset(reset), .fibo_series(out));
+  
+  initial
+    begin
+      $dumpfile("dump.vcd");
+      $dumpvars(0);
+    end
+  
+  initial
+    begin
+      clk=0;
+      forever #5 clk=~clk;
+    end
+  
+  initial
+    begin
+      #10 reset=1'b0;
+      #20 reset=1'b1;
+      #10 reset=1'b0;
+      #300
+      $finish;
+    end
+  
+endmodule
