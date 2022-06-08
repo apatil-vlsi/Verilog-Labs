0e5bb1dbd56ccb0c7ace4200bcf430789c6c979f
git-anup <88287032+git-anup@users.noreply.github.com>
2022-06-09 01:40:04 +0530

    Create register_4b.v

---
+++ Fibonacci Series Generator/3. Hierarchical Fibonacci Generator/register_4b.v
@@ -0,0 +1,23 @@
+module register_4b(
+  input clk, reset,
+  input [3:0] reg_in,
+  output [3:0] reg_out,
+);
+  
+  reg [3:0] d;
+  
+  dff dff1 (.clk(clk), .reset(reset), .d(d[0]), .q(reg_out[0]));
+  dff dff1 (.clk(clk), .reset(reset), .d(d[1]), .q(reg_out[1]));
+  dff dff1 (.clk(clk), .reset(reset), .d(d[2]), .q(reg_out[2]));
+  dff dff1 (.clk(clk), .reset(reset), .d(d[3]), .q(reg_out[3]));
+  
+  always @ (posedge clk)
+    if (clk)
+      begin
+        d[0] = reg_in[0];
+        d[1] = reg_in[1];
+        d[2] = reg_in[2];
+        d[3] = reg_in[3];
+      end
+  
+endmodule
