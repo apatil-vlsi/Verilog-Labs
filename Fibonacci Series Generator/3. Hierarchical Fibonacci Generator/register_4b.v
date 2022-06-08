d3661020592289dfc638b5242ac707aa6ef86f1b
git-anup <88287032+git-anup@users.noreply.github.com>
2022-06-09 02:11:16 +0530

    Update register_4b.v

--- Fibonacci Series Generator/3. Hierarchical Fibonacci Generator/register_4b.v
+++ Fibonacci Series Generator/3. Hierarchical Fibonacci Generator/register_4b.v
@@ -1,15 +1,17 @@
-module register_4b(
+module register_4b
+  #(parameter [3:0] rst_value = 4'b0000)
+  (
   input clk, reset,
   input [3:0] reg_in,
-  output [3:0] reg_out
-);
+    output [3:0] reg_out
+  );
   
   reg [3:0] d;
   
-  dff #(1'b1) dff1 (.clk(clk), .reset(reset), .d(d[0]), .q(reg_out[0]));
-  dff #(1'b0) dff2 (.clk(clk), .reset(reset), .d(d[1]), .q(reg_out[1]));
-  dff #(1'b0) dff3 (.clk(clk), .reset(reset), .d(d[2]), .q(reg_out[2]));
-  dff #(1'b0) dff4 (.clk(clk), .reset(reset), .d(d[3]), .q(reg_out[3]));
+  dff #(.rst_value(rst_value[0])) dff1 (.clk(clk), .reset(reset), .d(d[0]), .q(reg_out[0]));
+  dff #(.rst_value(rst_value[1])) dff2 (.clk(clk), .reset(reset), .d(d[1]), .q(reg_out[1]));
+  dff #(.rst_value(rst_value[2])) dff3 (.clk(clk), .reset(reset), .d(d[2]), .q(reg_out[2]));
+  dff #(.rst_value(rst_value[3])) dff4 (.clk(clk), .reset(reset), .d(d[3]), .q(reg_out[3]));
   
   always @ (posedge clk)
     if (clk)
