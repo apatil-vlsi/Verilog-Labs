6041471823490aec341bd96fac6cd5eb20870007
git-anup <88287032+git-anup@users.noreply.github.com>
2022-06-09 00:57:29 +0530

    Update fibo_gen_top.v

--- Fibonacci Series Generator/3. Hierarchical Fibonacci Generator/fibo_gen_top.v
+++ Fibonacci Series Generator/3. Hierarchical Fibonacci Generator/fibo_gen_top.v
@@ -1,1 +1,13 @@
-
+module fibo_gen_top(
+  input clk, reset,
+  output reg fibo_series
+);
+  
+  wire  add_out, cout;
+	reg   add_in1;
+  
+  adder_4b fibo_add (.A(add_in1), .B(add_in2), .cout(cout), .sum(add_out));
+  register_4b fibo_reg1 (.clk(clk), .reset(reset), .reg_in(add_out), .reg_out(add_in1));
+  register_4b fibo_reg2 (.clk(clk), .reset(reset), .reg_in(add_in1), .reg_out(fibo_series));
+  
+endmodule
