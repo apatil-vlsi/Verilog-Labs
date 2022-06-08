5430beeef58c72064c67e27e9ec24fd504a38e3b
git-anup <88287032+git-anup@users.noreply.github.com>
2022-06-09 02:30:46 +0530

    Update fibo_gen_top.v

--- Fibonacci Series Generator/3. Hierarchical Fibonacci Generator/fibo_gen_top.v
+++ Fibonacci Series Generator/3. Hierarchical Fibonacci Generator/fibo_gen_top.v
@@ -7,7 +7,7 @@ module fibo_gen_top(
   wire [3:0]  add_out;
   reg  [3:0]  add_in1;
   
-  rcadder_4b fibo_add (.a(add_in1), .b(add_in2), .cout(cout), .sum(add_out));
+  rcadder_4b fibo_add (.a(add_in1), .b(fibo_series), .cout(cout), .sum(add_out));
   register_4b #(.rst_value(4'b0001)) fibo_reg1 (.clk(clk), .reset(reset), .reg_in(add_out), .reg_out(add_in1));
   register_4b #(.rst_value(4'b0000)) fibo_reg2 (.clk(clk), .reset(reset), .reg_in(add_in1), .reg_out(fibo_series));
   
